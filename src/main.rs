b2495cee5b4a925cb1cbed1447de3203634de2b6
simon <yconsj@gmail.com>
2023-08-08 13:46:29 +0200

    introduction

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// https://os.phil-opp.com/minimal-rust-kernel/
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
